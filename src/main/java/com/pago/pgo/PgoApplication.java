3e77ade21d40620913c15474fd383afaa1db39d0
ldavidtl <torres_d44@yahoo.es>
2024-05-11 22:50:46 -0600

    initial commit

---
+++ src/main/java/com/pago/pgo/PgoApplication.java
@@ -0,0 +1,13 @@
+package com.pago.pgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgoApplication.class, args);
+	}
+
+}
